85d7ebeb0627856fe242db73be3fba45a2642adb
SAFRAN <mohammedsafran6ix@gmail.com>
2024-10-12 02:25:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var sidenavbar = document.querySelector(".side-navbar");
+
+function open_nav_box(){
+    sidenavbar.style.top = "0px"
+}
+function close_nav_bar(){
+    sidenavbar.style.top = "-500px"
+}
