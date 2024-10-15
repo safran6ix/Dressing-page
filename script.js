b1b4b35dfd5577bd07df929a3d170621acb4c9ca
safran6ix <mohammedsafran6ix@gmail.com>
2024-10-15 17:41:05 +0530

    modified

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var sidenavbar = document.querySelector(".side-navbar");
+var cart = document.querySelector(".cart");
 
 function open_nav_box(){
     sidenavbar.style.top = "0px"
+    cart.style.display = "none"
 }
 function close_nav_bar(){
     sidenavbar.style.top = "-500px"
+    cart.style.display = "block"
 }
